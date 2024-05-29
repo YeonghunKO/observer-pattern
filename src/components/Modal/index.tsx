860af9cb7e5aa914ae9dbece21f83381ae72c06e
Sinkyo <yhko1988@gmail.com>
2024-05-29 19:59:59 +0900

    feat : 모달 스타일 적용

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,3 +1,15 @@
+import { useEffect, useState } from "react";
+import Style from "./Modal.module.css";
+
 export const Modal = () => {
-  return <div>Modal</div>;
+  const [aniClass, setAniClassName] = useState("");
+
+  useEffect(() => {
+    setAniClassName(Style["move-up"]);
+    setTimeout(() => {
+      setAniClassName(Style["move-down"]);
+    }, 1000);
+  }, []);
+
+  return <div className={`${Style.modal} ${aniClass}`}>Modal</div>;
 };
