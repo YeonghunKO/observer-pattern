bd6576f3e381eed01c6aafff96acdc90d758cf94
Sinkyo <yhko1988@gmail.com>
2024-05-29 20:47:07 +0900

    feat : 토스트 먹기

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Module from "./App.module.css";
+import { createPortal } from "react-dom";
+import { Modal } from "./components/Modal";
+import { showToast } from "./utils/showToast";
 import Nested from "./components/Nested";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+    showToast("토스트 내놔 🥪");
+  };
+
+  const handleModalClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
-      <button type="button">클릭해보렴</button>
+      <button onClick={handleClick} type="button">
+        클릭해보렴
+      </button>
       <Nested />
+      {createPortal(
+        <Modal onClose={handleModalClose} open={open} />,
+        document.getElementById("portal") as HTMLDivElement
+      )}
     </>
   );
 }
