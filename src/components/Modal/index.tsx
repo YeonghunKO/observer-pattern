bd151f815b3db8eaeeae71ec94bd0fd69810a5a9
Sinkyo <yhko1988@gmail.com>
2024-05-29 20:46:57 +0900

    refactor : open , close 함수 리팩토링

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,15 +1,33 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import Style from "./Modal.module.css";
+import { useToast } from "../../hooks/useToast";
 
-export const Modal = () => {
+export const Modal = ({
+  open,
+  onClose,
+}: {
+  open: boolean;
+  onClose: () => void;
+}) => {
   const [aniClass, setAniClassName] = useState("");
+  const toasts = useToast();
+  const handleClose = () => {
+    setAniClassName(Style["move-down"]);
+    onClose();
+  };
 
   useEffect(() => {
-    setAniClassName(Style["move-up"]);
-    setTimeout(() => {
-      setAniClassName(Style["move-down"]);
-    }, 1000);
-  }, []);
+    if (open) {
+      setAniClassName(Style["move-up"]);
+    }
+  }, [open]);
 
-  return <div className={`${Style.modal} ${aniClass}`}>Modal</div>;
+  return (
+    <div className={`${Style.modal} ${aniClass}`}>
+      {toasts.map((toast) => (
+        <p key={toast.id}>{toast.value}</p>
+      ))}
+      <button onClick={handleClose}>close</button>
+    </div>
+  );
 };
