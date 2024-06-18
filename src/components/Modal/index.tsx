b38f97cd6e8911cac9546c9d3f153c894bc39b69
Sinkyo <yhko1988@gmail.com>
2024-06-18 12:30:43 +0900

    refactor : 모달은 애니메이션하고 메시지만 담당
    - SRP를 따르게 함

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,33 +1,21 @@
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Style from "./Modal.module.css";
-import { useToast } from "../../hooks/useToast";
 
-export const Modal = ({
-  open,
-  onClose,
-}: {
-  open: boolean;
-  onClose: () => void;
-}) => {
-  const [aniClass, setAniClassName] = useState("");
-  const toasts = useToast();
-  const handleClose = () => {
-    setAniClassName(Style["move-down"]);
-    onClose();
-  };
+export const Modal = ({ message }: { message: string }) => {
+  if (!message) {
+    return null;
+  }
+  const [aniClass, setAniClassName] = useState(Style["move-up"]);
 
   useEffect(() => {
-    if (open) {
-      setAniClassName(Style["move-up"]);
-    }
-  }, [open]);
+    setTimeout(() => {
+      setAniClassName(Style["move-down"]);
+    }, 1000);
+  }, []);
 
   return (
     <div className={`${Style.modal} ${aniClass}`}>
-      {toasts.map((toast) => (
-        <p key={toast.id}>{toast.value}</p>
-      ))}
-      <button onClick={handleClose}>close</button>
+      <p>{message}</p>
     </div>
   );
 };
